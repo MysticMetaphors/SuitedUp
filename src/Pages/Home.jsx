be5b871c3c31d73c19dd67ba62ed3e06d08a4f7d
VonBryan <vonbanalbryan18v@gmail.com>
2025-08-12 13:24:28 +0800

    Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,91 @@
+import SuitedButton from "../Components/SuitedComps/SuitedButton"
+
 export default function Home() {
     return (
         <>
-        Home
+            <div className="w-full h-full px-35">
+                {/* <div className="grid grid-cols-1 md:grid-cols-2 items-center"> */}
+                <div className=" mt-20 text-center">
+                    <h1 className="mb-4 bg-clip-text text-transparent text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl"
+                        style={{
+                            backgroundImage: 'repeating-linear-gradient(to right, #E8C068 30%, #D19C46 25%, #E8C068 50%, #D19C46 75%)'
+                        }}>
+                        The menswear label setting Gold standard for timeless tuxedos
+                    </h1>
+                    <p className="mb-6 mx-50 text-lg font-normal text-primaryGrey-1000 lg:text-xl">Designed for life’s defining moments, our tuxedos combine masterful tailoring, premium fabrics, and modern sophistication to ensure you stand apart in every room.</p>
+                    <SuitedButton text="Buy Item" icon="shopping_bag" />
+                    <SuitedButton text="Rent Item" theme="secondary" />
+                </div>
+                {/* <div></div>
+                </div> */}
+                <div className="mt-15 flex gap-[8px]">
+                    <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </a>
+                    <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </a>
+                    <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </a>
+                    <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </a>
+                </div>
+                <div className="mt-23">
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                        <div className="grid gap-4">
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="" />
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="" />
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
+                            </div>
+                        </div>
+                        <div className="grid gap-4">
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="" />
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="" />
+                            </div>
+                        </div>
+                        <div className="grid gap-4">
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="" />
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="" />
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="" />
+                            </div>
+                        </div>
+                        <div className="grid gap-4">
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="" />
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="" />
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="" />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
